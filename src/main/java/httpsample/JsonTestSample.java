9a5e9999d15dce6eac3472a51676e14aa3eba37b
zlt633p <zlt633p@gmail.com>
2016-06-13 10:02:25 -0400

    more examples

--- src/main/java/httpsample/JsonTestSample.java
+++ src/main/java/httpsample/JsonTestSample.java
@@ -3,12 +3,14 @@ package httpsample;
 import java.io.IOException;
 import java.util.Map;
 
-import org.apache.http.client.ClientProtocolException;
 import org.apache.http.client.methods.CloseableHttpResponse;
 import org.apache.http.client.methods.HttpGet;
 import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClients;
 import org.apache.log4j.Logger;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
 
 import util.HttpUtil;
 import util.Json2Codec;
@@ -16,6 +18,11 @@ import util.Json2Codec;
 /**
  * @author ztang
  * generated at : Jun 10, 2016 - 9:34:54 AM
+ * 
+ * Refer to: http://www.jsontest.com/#headers
+ * Cross-Origin Resource Sharing: https://www.w3.org/TR/cors/
+ * HTTP access control (CORS): https://developer.mozilla.org/en-US/docs/Web/HTTP/Access_control_CORS
+ * json Media Type for JavaScript Object Notation: http://www.ietf.org/rfc/rfc4627.txt
  */
 public class JsonTestSample {	
 	private static final Logger logger = Logger.getLogger(HttpUtil.class);
@@ -26,13 +33,53 @@ public class JsonTestSample {
 		init();
 	}
 	
+	@Before
 	public void init(){
 		httpClient = HttpClients.createDefault();		
 	}
 	
-	public void run_Date(){
-		String urltext = "http://date.jsontest.com";
+	@Test
+	public void run_IpAddress(){
+		String urltext = "http://ip.jsontest.com/";
 		
+		HttpGet httpGet = new HttpGet(urltext);		
+		CloseableHttpResponse httpRes = null;
+		try {
+			httpRes = httpClient.execute(httpGet);
+			String resContent = HttpUtil.getHttpResponseContent(httpRes);			
+			//logger.info(resContent);
+			
+			Map<String, Object> convertedMap = Json2Codec.unmarshalToMap(resContent);
+			logger.info(convertedMap);
+		} catch (IOException e) {
+			logger.warn(e.getMessage(), e);
+		} finally {
+			HttpUtil.close(httpRes);
+		} 
+	}		
+	
+	@Test
+	public void run_Date(){
+		String urltext = "http://date.jsontest.com";		
+		HttpGet httpGet = new HttpGet(urltext);		
+		CloseableHttpResponse httpRes = null;
+		try {
+			httpRes = httpClient.execute(httpGet);
+			String resContent = HttpUtil.getHttpResponseContent(httpRes);			
+			logger.info(resContent);
+			
+			Map<String, Object> convertedMap = Json2Codec.unmarshalToMap(resContent);
+			logger.info(convertedMap);
+		} catch (IOException e) {
+			logger.warn(e.getMessage(), e);
+		} finally {
+			HttpUtil.close(httpRes);
+		} 
+	}
+	
+	@Test
+	public void run_EchoJSON(){
+		String urltext = "http://echo.jsontest.com/key/value/one/two/xx/yy";		
 		HttpGet httpGet = new HttpGet(urltext);		
 		CloseableHttpResponse httpRes = null;
 		try {
@@ -50,6 +97,46 @@ public class JsonTestSample {
 	}
 	
 	
+	@Test
+	public void run_HttpHeader(){
+		String urltext = "http://headers.jsontest.com/";		
+		HttpGet httpGet = new HttpGet(urltext);		
+		CloseableHttpResponse httpRes = null;
+		try {
+			httpRes = httpClient.execute(httpGet);
+			String resContent = HttpUtil.getHttpResponseContent(httpRes);			
+			logger.info(resContent);
+			
+			Map<String, Object> convertedMap = Json2Codec.unmarshalToMap(resContent);
+			logger.info(convertedMap);
+		} catch (IOException e) {
+			logger.warn(e.getMessage(), e);
+		} finally {
+			HttpUtil.close(httpRes);
+		} 
+	}
+	
+	@Test
+	public void run_Cookie(){
+		String urltext = "http://cookie.jsontest.com/";
+		
+		HttpGet httpGet = new HttpGet(urltext);		
+		CloseableHttpResponse httpRes = null;
+		try {
+			httpRes = httpClient.execute(httpGet);
+			String resContent = HttpUtil.getHttpResponseContent(httpRes);			
+			//logger.info(resContent);
+			
+			Map<String, Object> convertedMap = Json2Codec.unmarshalToMap(resContent);
+			logger.info(convertedMap);
+		} catch (IOException e) {
+			logger.warn(e.getMessage(), e);
+		} finally {
+			HttpUtil.close(httpRes);
+		} 
+	}
+	
+	@Test
 	public void run_MD5(){
 		String urltext = "http://md5.jsontest.com/?text=whatever-forboo";
 		
@@ -67,8 +154,28 @@ public class JsonTestSample {
 		} finally {
 			HttpUtil.close(httpRes);
 		} 
-	}	
+	}
 	
+	@Test
+	public void run_CallBack(){
+		String urltext = "http://ip.jsontest.com/?callback=showIP";
+		
+		HttpGet httpGet = new HttpGet(urltext);		
+		CloseableHttpResponse httpRes = null;
+		try {
+			httpRes = httpClient.execute(httpGet);
+			String resContent = HttpUtil.getHttpResponseContent(httpRes);			
+			logger.info(resContent);
+
+		} catch (IOException e) {
+			logger.warn(e.getMessage(), e);
+		} finally {
+			HttpUtil.close(httpRes);
+		} 
+	}
+	
+	
+	@After
 	public void destroy(){
 		HttpUtil.close(httpClient);
 	}
@@ -78,7 +185,7 @@ public class JsonTestSample {
 		JsonTestSample agent = new JsonTestSample();
 		
 		//agent.run_Date();
-		agent.run_MD5();
+		agent.run_HttpHeader();
 		
 		agent.destroy();
 		System.exit(0);
